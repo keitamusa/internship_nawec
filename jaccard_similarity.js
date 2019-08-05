d8dc9e6bf9eae10cfe0cbfda9c772924573d9cf4
Musa Keita <keitamusa@gmail.com>
2019-08-05 13:43:17 +0000

    Updating jaccard_similarity file and adding app.py to the repo

--- jaccard_similarity.js
+++ jaccard_similarity.js
@@ -13,17 +13,27 @@ function intersection(arr1, arr2) {
 }
 
 function A_plus_B(arr1, arr2) {
-
-  let num1 = arr1.unshift();
-  let num2 = arr2.unshift();
-  if (num1 != result && num2 != result) {
-    let total = 0;
-    total = num1 + num2;
-    return total;
-  }
+  // if (num1 != result && num2 != result) {
+  let num1 = arr1[Math.floor((Math.random() * arr1.length))];
+  let num2 = arr2[Math.floor((Math.random() * arr2.length))];
+  console.log(num1);
+  console.log(num2);
+  let total = num1 + num2;
+  return total;
+  // }
+  // let num1 = arr1.unshift();
+  // let num2 = arr2.unshift();
+  // if (num1 != result && num2 != result) {
+  //   let total = 0;
+  //   total = num1 + num2;
+  //   return total;
+  // }
 }
 
 let result = intersection([3, 8, 7, 5, 6, 2, 9], [3, 10, 8, 6, 6, 4, 5]);
+
 let ab = A_plus_B([3, 8, 7, 5, 6, 2, 9], [3, 10, 8, 6, 6, 4, 5]);
+
 let jaccard_similarity = result / (ab - result);
+console.log(result);
 console.log(jaccard_similarity);
